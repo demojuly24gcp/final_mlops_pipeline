2f44e904ac473a3242bad3b50b6643fab041e6c9
demojuly24gcp <edurekajuly24gcp@gmail.com>
2025-08-31 19:40:23 +0530

    check

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -95,7 +95,7 @@ class ModelTrainer:
                 'subsample':[0.6,0.7,0.75,0.85,0.9],
                 # 'criterion':['squared_error', 'friedman_mse'],
                 # 'max_features':['auto','sqrt','log2'],
-                'n_estimators': [8,16,32,64,128]
+                'n_estimators': [8,16,32,64,128,256]
             },
             "Logistic Regression":{},
             "AdaBoost":{
